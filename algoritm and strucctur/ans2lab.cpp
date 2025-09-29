deb9edbec6403a460e19846646ba031e6a2ce54a
kqlae <kqlae@DESKTOP-C2SHSNG>
2025-09-29 18:10:53 +0700

    индекс вхождения

--- algoritm and strucctur/ans2lab.cpp
+++ algoritm and strucctur/ans2lab.cpp
@@ -11,3 +11,38 @@ BadCharTable buildBadCharTable(const std::string& pattern) {
     }
     return table;
 }
+int findIndex(const std::string& text, const std::string& pattern) {
+    if (pattern.empty()) {
+        return 0; // Empty pattern
+    }
+    if (text.length() < pattern.length()) {
+        return -1; // Pattern cannot be found
+    }
+
+    BadCharTable badCharTable = buildBadCharTable(pattern);
+    int n = text.length();
+    int m = pattern.length();
+    int shift = 0;
+    while (shift <= (n - m)) {
+        int j = m - 1; // Start comparison from the end
+        while (j >= 0 && pattern[j] == text[shift + j]) {
+            j--;
+        }
+        if (j < 0) {
+            return shift; 
+        }
+        else {
+            char mismatchedChar = text[shift + j];
+            int badCharShift = 0;
+            if (badCharTable.count(mismatchedChar)) {
+                badCharShift = j - badCharTable[mismatchedChar];
+                if (badCharShift < 1) badCharShift = 1;
+            }
+            else {
+                badCharShift = j + 1;
+            }
+            shift += badCharShift; 
+        }
+    }
+    return -1; // Pattern not found.
+}
